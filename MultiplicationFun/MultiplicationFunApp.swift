342583c3c773b62dc0f5c61cf9175e12cfa82a60
Woodrow Martyr <woodrow.martyr@gmail.com>
2024-01-17 06:23:20 +1030

    Initial Commit

---
+++ MultiplicationFun/MultiplicationFunApp.swift
@@ -0,0 +1,17 @@
+//
+//  MultiplicationFunApp.swift
+//  MultiplicationFun
+//
+//  Created by Woodrow Martyr on 17/1/2024.
+//
+
+import SwiftUI
+
+@main
+struct MultiplicationFunApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
