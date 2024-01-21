99a3f10b6d56cd495606d284ce5e9d608356e43d
Woodrow Martyr <woodrow.martyr@gmail.com>
2024-01-22 06:24:54 +1030

    Fix bug where it does not determine the correct answer.

--- MultiplicationFun/ContentView.swift
+++ MultiplicationFun/ContentView.swift
@@ -12,50 +12,63 @@ struct ContentView: View {
     @State private var questionNumber = 5
     @State private var multiplicand = 2
     @State private var multiplier = 12
+    @State private var correctAnswerIndex = 0
     @State private var score = 0
     @State private var answerArray = [0, 0, 0]
-    @State private var correctAnswerIndex = 0
+    @State private var scoreTitle = ""
+    @State private var showingScore = false
+    @State private var endOfGame = false
     
     let questionNumbers = [5, 10, 20]
     
     var body: some View {
         NavigationStack {
-            Form {
-                Section {
+            VStack(spacing: 20) {
+                HStack {
+                    Text("Enter the tables to practice")
                     Picker("Enter the tables to practice", selection: $highestMultiplicationTable) {
                         ForEach(2..<13) {
                             Text("Up to \($0)")
                         }
                     }
                     .onChange(of: highestMultiplicationTable){
-                        generateNumbers()
-                        generateAnswerArray()
+                        newQuestion()
                     }
-                    Text("number is: \(highestMultiplicationTable + 2)")
                 }
-                Section("How many questions do you want") {
-                    Picker("Question number", selection: $questionNumber) {
-                        ForEach(questionNumbers, id: \.self) {
-                            Text(String($0))
-                        }
+                Text("How many questions do you want")
+                Picker("Question number", selection: $questionNumber) {
+                    ForEach(questionNumbers, id: \.self) {
+                        Text(String($0))
                     }
-                    .pickerStyle(.segmented)
                 }
-                Section {
-                    Text("\(multiplicand) x \(multiplier) = ?")
-                }
-                HStack(spacing: 20) {
-                    ForEach(0..<3) { answerIndex in
+                .pickerStyle(.segmented)
+                Text("\(multiplicand) x \(multiplier) = ?")
+                Spacer()
+                HStack() {
+                    ForEach(0..<3) { number in
                         Button {
+                            buttonTapped(number)
                         } label: {
-                            Text("\(answerArray[Int(answerIndex)])")
+                            Text("\(answerArray[Int(number)])")
                                 .frame(width: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, height: 100)
                                 .background(.red)
                                 .foregroundStyle(.white)
+                                .clipShape(.rect(cornerRadius: 20))
                         }
                     }
                 }
+                Spacer()
             }
+            .navigationTitle("Multiplication Fun")
+            .padding(30)
+        }
+
+
+        .onAppear(perform: newQuestion)
+        .alert(scoreTitle, isPresented: $showingScore) {
+            Button("Continue", action: newQuestion)
+        } message: {
+            Text("Your score is \(score)")
         }
     }
     
@@ -66,11 +79,11 @@ struct ContentView: View {
     
     func generateAnswerArray() {
         let correctAnswer = multiplicand * multiplier
+        correctAnswerIndex = Int.random(in: 0..<3)
         
         var randomAnswer = 0
         
         answerArray = []
-        correctAnswerIndex = Int.random(in: 0..<3)
         
         for i in 0..<3 {
             randomAnswer = randomValidAnswer()
@@ -89,6 +102,24 @@ struct ContentView: View {
     
     func randomValidAnswer() -> Int {
         return Int.random(in: 2...((highestMultiplicationTable + 2) * 12))
+    }
+    
+    func newQuestion() {
+        generateNumbers()
+        generateAnswerArray()
+    }
+    
+    func buttonTapped(_ chosenIndex: Int) {
+        print("Button \(chosenIndex) tapped.")
+        print("chosenIndex: \(chosenIndex)")
+        print("correctAnswerIndex: \(correctAnswerIndex)")
+        if chosenIndex == correctAnswerIndex {
+            scoreTitle = "Correct"
+            score += 1
+        } else {
+            scoreTitle = "Correct answer is \(answerArray[correctAnswerIndex])"
+        }
+        showingScore = true
     }
 }
 
